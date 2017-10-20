9748b491a9e00ec828ac0416c7113c66e42f8c32
Araba Taylor <araba.taylor20@gmail.com>
2017-10-20 02:13:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,6 +22,10 @@ class School
   end
 
   def sort
-    @roster.values.sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
   end
 end
