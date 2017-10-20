5781f181ef20ace08b8c503fd7b7b6cc5f343773
Araba Taylor <araba.taylor20@gmail.com>
2017-10-20 02:12:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @school_name=school_name
+    @roster={}
+  end
+
+  def add_student(name, grade)
+    if self.roster.keys.include?(grade) == false
+        self.roster[grade] = []
+        self.roster[grade] << name
+    else
+      self.roster[grade] << name
+    end
+  end
+#want roster[grade]=> array of names
+  def grade(i)
+    @roster[i]
+  end
+
+  def sort
+    @roster.grade.sort
+  end
+end
